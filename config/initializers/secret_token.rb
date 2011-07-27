f081422d79dd455cf138a2d3442420ea0e23fd38
pinetail <civic.typexs@gmail.com>
2011-07-27 12:01:59 +0900

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParkingNaviServer::Application.config.secret_token = '7a613ceea7d2df346c0f80816e45875b79c5c910fa2efd75819a2ea5990f6ddd8ebde92fa42edc0b3665b552142caf2f7620435e8db592f742c9af526d928e5a'
